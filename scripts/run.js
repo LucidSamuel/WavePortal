7891da339e285160ff524a5d109d7cdf38c5c784
Samuel Akinosho <samuelakins390@gmail.com>
2022-07-04 17:08:31 +0400

    smart contract update

--- scripts/run.js
+++ scripts/run.js
@@ -1,25 +1,22 @@
 const main = async () => {
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-  const waveContract = await waveContractFactory.deploy({
-      value: hre.ethers.utils.parseEther("0.1"),
-  });
+  const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
   console.log("Contract addy:", waveContract.address);
 
-  let contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
-  console.log("Contract balance:", hre.ethers.utils.formatEther(contractBalance));
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
 
-  /*
-   * Let's try two waves now
+  /**
+   * Let's send a few waves!
    */
-  const waveTxn = await waveContract.wave("This is wave #1");
-  await waveTxn.wait();
+  let waveTxn = await waveContract.wave("A message!");
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-  const waveTxn2 = await waveContract.wave("This is wave #2");
-  await waveTxn2.wait();
-
-  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
-  console.log("Contract balance:", hre.ethers.utils.formatEther(contractBalance));
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
   let allWaves = await waveContract.getAllWaves();
   console.log(allWaves);
@@ -27,11 +24,11 @@ const main = async () => {
 
 const runMain = async () => {
   try {
-      await main();
-      process.exit(0);
+    await main();
+    process.exit(0);
   } catch (error) {
-      console.log(error);
-      process.exit(1);
+    console.log(error);
+    process.exit(1);
   }
 };
 
